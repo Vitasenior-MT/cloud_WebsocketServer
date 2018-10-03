2c84d5f0fee1aeffb1031f0149109fc0514ebd57
diogosantosmendes <diogosantosmendes@hotmail.com>
2018-10-03 15:20:52 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// require('sticky-cluster')(
+//   (callback) => {
+//     Promise.all([
+//       require("./src/broker_connect").connect(),
+//       require('./src/models/index').sequelize.sync()
+//     ]).then(
+//       res => {
+//         let channel = res[0];
+//         console.log('\x1b[32m%s\x1b[0m.', '(PLAIN) Connection established with MongoDB, MySQL and RabbitMQ');
+
+//         // This code will be executed only in slave workers
+//         let express = require('express');
+//         var server = require('http').createServer(express());
+
+//         //Handle messaging
+//         require('./src/message_handler').listen(require('socket.io')(server), channel);
+
+//         callback(server);
+
+//       }, error => console.log(error));
+//   },
+//   {
+//     port: process.env.PORT || 8000
+//   }
+// );
+
+
+Promise.all([
+  require("./src/broker_connect").connect(),
+  require('./src/models/index').sequelize.sync()
+]).then(
+  res => {
+    console.log('\x1b[32m%s\x1b[0m.', '(PLAIN) Connection established with MySQL and RabbitMQ');
+
+    // This code will be executed only in slave workers
+    let express = require('express');
+    var server = require('http').createServer(express());
+
+    // Handle messaging
+    require('./src/broker_websocket').listen(require('socket.io')(server), res[0]);
+
+    // Start ws server
+    let port = process.env.PORT || 8008;
+    server.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+
+  }, error => { console.log('Unable to connect to Databases and broker server.', error); process.exit(1); });
+
---
+++ src/broker_websocket.js
@@ -0,0 +1,68 @@
+// https://github.com/john-pettigrew/scaling-socket-io-talk/blob/master/code/app.js
+exports.listen = (socketIO, channel) => {
+  require("./business/get_vitaboxes").list().then(
+    vitaboxes => {
+      vitaboxes.push("admin");
+      Promise.all(vitaboxes.map(vitabox => _subscribe(channel, vitabox))).then(
+        exchanges => {
+          exchanges.forEach(exchange => exchange.onMessageReceived = onMessageReceived);
+
+          socketIO.on('connection', (socket) => {
+            if (socket.handshake.query && socket.handshake.query.token) {
+              require("./business/validate_token").validateToken(socket.handshake.query.token).then(
+                boxes => {
+                  socket.join(boxes, (err) => {
+                    if (err) socket.emit("message", "error on join rooms: " + err.message);
+                    else socketIO.to(boxes[0]).emit('message', "Hello from server");
+
+                    console.log("\x1b[32mnew connection\x1b[0m assigned to rooms: ", Object.keys(socket.rooms));
+
+                    socket.on('disconnect', () => { console.log("\x1b[35muser disconect\x1b[0m: ", socket.conn.id); });
+                  })
+                },
+                error => { socket.emit("message", "Unauthorized: " + error.message); socket.disconnect(true) });
+            } else { socket.emit("message", "Unauthorized: token undefined"); socket.disconnect(true); }
+          });
+
+          function onMessageReceived(room, message) {
+            socketIO.to(room).emit('message', message);
+          }
+
+          console.log('\x1b[32m%s %s\x1b[0m', '(PLAIN) Worker ', process.pid, 'listening rooms')
+        }, error => console.log(error))
+    }, error => console.log(error));
+}
+
+// PRIVATE
+//_________________________
+_subscribe = (channel, vitabox_id) => {
+  return new Promise((resolve, reject) => {
+
+    channel.assertExchange(vitabox_id, 'fanout', { durable: true });
+
+    //setup a queue for receiving messages
+    channel.assertQueue('', { exclusive: true }, function (err, q) {
+      if (err) reject(err);
+
+      channel.bindQueue(q.queue, vitabox_id, '');
+
+      let exchanges = {
+        emitMessage: emitMessage,
+        onMessageReceived: onMessageReceived
+      };
+
+      //listen for messages
+      channel.consume(q.queue, function (msg) {
+        exchanges.onMessageReceived(vitabox_id, msg.content.toString());
+      }, { noAck: true });
+
+      function emitMessage(message) {
+        channel.publish(vitabox_id, '', new Buffer(message));
+      }
+      function onMessageReceived() { }
+
+      resolve(exchanges);
+
+    });
+  });
+}
